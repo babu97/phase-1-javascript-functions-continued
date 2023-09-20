64de8203e8a1658144a3b7f95745ff275eddd8a2
babu97 <benkaimugul@gmail.com>
2023-09-20 20:24:42 +0300

    finished wrapadjective content

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+
+function mondayWork(activity = 'go to the office' ){
+    return `This Monday, I will ${activity}.`
+
+}
+
+function wrapAdjective(outerSymbol) {
+    return function(adjective = "special") {
+        return `You are ${outerSymbol}${adjective}${outerSymbol}!`;
+    };
+  }
+  const test1 = wrapAdjective("*")("a hard worker");
+console.log(test1); // Output: "You are *a hard worker*!"
